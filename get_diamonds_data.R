e7806c6bfd0be909aa8a24e62626702176dd0d90
nglvnt <nglvnt@protonmail.com>
2022-07-07 10:58:23 +0200

    Leave dimension-related columns out of the data

--- get_diamonds_data.R
+++ get_diamonds_data.R
@@ -7,4 +7,8 @@ data(diamonds, package = "ggplot2")
 ## we only would like to analyze diamonds of weight at least 1 carat
 diamonds <- diamonds[diamonds$carat >= 1, ]
 
+# leave out the columns related to the dimension of the diamonds:
+# depth, table, x, y, z
+diamonds <- diamonds[c("carat", "cut", "color", "clarity", "price")]
+
 write.csv(x = diamonds, file = "data/diamonds.csv", row.names = FALSE)
